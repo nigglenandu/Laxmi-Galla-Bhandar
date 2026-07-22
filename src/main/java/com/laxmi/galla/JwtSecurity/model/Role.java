1dcdaf3a617d18de156ab67601646172b7e1adcb
[Niggle Nandu] <nigglenandu@gmail.com>
2026-07-22 17:29:06 +0545

    jwt commited

--- src/main/java/com/laxmi/galla/JwtSecurity/model/Role.java
+++
@@ -1,9 +0,0 @@
-package com.laxmi.galla.JwtSecurity.model;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_VENDOR,
-    ROLE_DELIVERY,
-    ROLE_SUPPORT
-}
--- src/main/java/com/laxmi/galla/JwtSecurity/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.laxmi.galla.JwtSecurity.repository;
-
-import com.laxmi.galla.JwtSecurity.model.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-}
